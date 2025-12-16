ed2e358de7fc75d0d72a948bb6ea152006fbcdff
vincenzrey <144880422+vincenzrey@users.noreply.github.com>
2025-12-16 16:54:48 +0800

    Update automate_Vincenz-Reynard-Citro.py

--- preprocessing/automate_Vincenz-Reynard-Citro.py
+++ preprocessing/automate_Vincenz-Reynard-Citro.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-
+import argparse
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 
@@ -12,19 +12,10 @@ def preprocess_data(
     test_size: float = 0.2,
     random_state: int = 42
 ):
-    """
-    Fungsi untuk melakukan preprocessing data secara otomatis
-    dan menyimpan dataset yang siap dilatih.
-    """
-
-    # ========================
     # 1. Load Dataset
-    # ========================
     data = pd.read_csv(input_path)
 
-    # ========================
     # 2. Encoding Kategorikal
-    # ========================
     cat_columns = data.select_dtypes(include='object').columns
     cat_columns = cat_columns.drop(target_column, errors='ignore')
 
@@ -32,25 +23,18 @@ def preprocess_data(
     for col in cat_columns:
         data[col] = encoder.fit_transform(data[col])
 
-    # ========================
     # 3. Normalisasi Numerik
-    # ========================
     numeric_columns = data.select_dtypes(include=['int64', 'float64']).columns
     scaler = MinMaxScaler()
     data[numeric_columns] = scaler.fit_transform(data[numeric_columns])
 
-    # ========================
     # 4. Pisahkan X dan y
-    # ========================
     X = data.drop(columns=[target_column])
     y = data[target_column]
 
-    # Encode target
     y = LabelEncoder().fit_transform(y)
 
-    # ========================
     # 5. Train-Test Split
-    # ========================
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
@@ -59,9 +43,7 @@ def preprocess_data(
         stratify=y
     )
 
-    # ========================
     # 6. Simpan Dataset
-    # ========================
     os.makedirs(output_dir, exist_ok=True)
 
     X_train.to_csv(f"{output_dir}/X_train.csv", index=False)
@@ -75,9 +57,14 @@ def preprocess_data(
 
     print("Preprocessing selesai. Dataset siap dilatih.")
 
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Automated preprocessing")
+    parser.add_argument("--input", type=str, required=True)
+    parser.add_argument("--output", type=str, required=True)
+
+    args = parser.parse_args()
+
     preprocess_data(
-        input_path="F:\Kuliah\Semester 5 (asah)\SMSML_Vincenz-Reynard-Citro\dataset_raw\obesity_data.csv",
-        output_dir="F:\Kuliah\Semester 5 (asah)\SMSML_Vincenz-Reynard-Citro\preprocessing\dataset_preprocessing"
+        input_path=args.input,
+        output_path=args.output
     )
