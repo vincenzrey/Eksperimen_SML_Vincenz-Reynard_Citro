3be219c67eb0546c612e4254a369896abb8138ec
vincenzrey <144880422+vincenzrey@users.noreply.github.com>
2025-12-17 20:37:48 +0800

    Update automate_Vincenz-Reynard-Citro.py

--- preprocessing/automate_Vincenz-Reynard-Citro.py
+++ preprocessing/automate_Vincenz-Reynard-Citro.py
@@ -27,7 +27,7 @@ def preprocess_data(
     scaler = MinMaxScaler()
     df[numeric_columns] = scaler.fit_transform(df[numeric_columns])
 
-    # 4. Simpan dataset hasil preprocessing (BELUM split)
+    # 4. Simpan dataset hasil preprocessing 
     os.makedirs(output_path, exist_ok=True)
     output_file = os.path.join(output_path, "obesity_preprocessed.csv")
     df.to_csv(output_file, index=False)
@@ -47,3 +47,4 @@ if __name__ == "__main__":
         input_path=args.input,
         output_path=args.output
     )
+
